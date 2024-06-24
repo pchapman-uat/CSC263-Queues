c3b1587fc2359b124c190756e392a7c6efbcc5f7
Preston Chapman <pchapman82070@uat.edu>
2024-06-24 13:03:09 -0700

    Add Queue class

--- Main.java
+++ Main.java
@@ -23,6 +23,28 @@ class StackUsingLinkedList<T>{
     }
 }   
 
+class QueueUsingLinkedList<T>{
+    private LinkedList<T> list = new LinkedList<>();
+
+    public void enqueue(T item){
+        list.addLast(item);
+    }
+
+    public T dequeue(){
+        this.isEmpty();
+        return list.removeFirst();
+    }
+
+    public T peek(){
+        this.isEmpty();
+        return list.getFirst();
+    }
+
+    private void isEmpty(){
+        if(list.isEmpty()) throw new IllegalStateException("Queue is Empty");
+    }
+}
+
 
 public class Main {
     public static void main(String[] args) {
