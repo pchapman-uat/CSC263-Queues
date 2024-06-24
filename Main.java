5c1e28ea45fb3aed6a16d177ac3a04017522e948
Preston Chapman <pchapman82070@uat.edu>
2024-06-24 12:54:04 -0700

    Begin Stack class

--- Main.java
+++ Main.java
@@ -1,3 +1,17 @@
+import java.util.LinkedList;
+
+class StackUsingLinkedList<T>{
+    private LinkedList<T> list = new LinkedList<>();
+    public void push(T item){
+        list.add(item);
+    }
+    public T pop(){
+        if(list.isEmpty()){
+            throw new IllegalStateException("Stack is Empty");
+        } return list.removeFirst();
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
         
